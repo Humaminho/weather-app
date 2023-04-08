e2cb2ed63df7e2541872bf3cdabd4adc12b96c27
humam <hu666mam@gmail.com>
2023-04-08 01:50:08 +0000

    stuff

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import './styles.css';
 const log = console.log;
-const temperatureEl = document.getElementById('temperature');
-const humidityEl = document.getElementById('humidity');
-const windSpeedEl = document.getElementById('wind-speed');
-const descriptionEl = document.getElementById('description');
 const inputEl = document.getElementById('location');
 const submit = document.getElementById('submit');
 const form = document.getElementById('form')
-const errorEl = document.getElementById('error')
-const emptyEl = document.getElementById('empty')
+const forecastEl = document.getElementById('forecast');
+
+forecastEl.addEventListener('click', getForecast('tiflet'));
 
 log('-- CONSOLE --');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 })
-submit.addEventListener('click', search);
+submit.addEventListener('click', () => {
+  if (inputEl.value) {
+    const location = inputEl.value;
+    getCurrentWeather(location);
+  } else return;
+});
 
-function search() {
-  const city = inputEl.value;
-  if (inputEl.value === "") {
-    return
-  }
+function getCurrentWeather(location) {
 
-  const path = `http://api.weatherapi.com/v1/current.json?key=1c22c861d3d140b58e7202945230604&q=${city}`
-  log(path)
+  const path = `http://api.weatherapi.com/v1/current.json?key=1c22c861d3d140b58e7202945230604&q=${location}`
+
   fetch(path, { mode: 'cors' })
   .then((response) => {
     if (!response.ok) {
@@ -33,9 +31,11 @@ function search() {
     return response.json();
   })
   .then((response) => {
+    displayCurrentWeather(response);
+    const errorEl = document.getElementById('error')
+    const emptyEl = document.getElementById('empty')
     errorEl.classList.add('hidden');
     emptyEl.classList.add('hidden');
-    displayInfo(response);
   })
   .catch((error) => {
     log(error)
@@ -46,17 +46,41 @@ function search() {
 
 }
 
-function displayInfo(response) {
-  log(response);
-  temperatureEl.innerText = response.current.temp_c + " °C";
-  humidityEl.innerText = response.current.humidity + " %";
-  windSpeedEl.innerText = response.current.wind_kph + " km/h"
-  descriptionEl.innerText = description(response);
+function getForecast(location) {
+
+  const path = `http://api.weatherapi.com/v1/forecast.json?key=1c22c861d3d140b58e7202945230604&q=${location}`
+
+  fetch(path, { mode: 'cors' })
+  .then((response) => {
+    log(response.json())
+  })
 }
+
+function displayCurrentWeather(response) {
+  const section = document.querySelector('.section');
+  section.innerText = "";
+  const currentSection = document.createElement('div');
+  currentSection.classList.add('current');
+  currentSection.innerHTML = `
+  <p id="title"> The weather today:</p>
+  <p id="error" class="hidden">Oops, something went wrong! Please try again.</p>
+  <p id="empty">Insert your city name</p>
+  <p id="description">${description(response)}</p>
+  <p id="temperature">${response.current.temp_c} °C</p>
+  <p id="humidity">${response.current.humidity} %</p>
+  <p id="wind-speed">${response.current.wind_kph} km/h</p>`
+  section.appendChild(currentSection);
+}
+
 function description(r) {
   return `It's currently ${r.current.condition.text} in ${r.location.name}, the temperature is ${r.current.temp_c} °C, and ${r.current.wind_kph} km/h winds.`;
 }
+
 function clear() {
+  const temperatureEl = document.getElementById('temperature');
+  const windSpeedEl = document.getElementById('wind-speed');
+  const descriptionEl = document.getElementById('description');
+  const humidityEl = document.getElementById('humidity');
   temperatureEl.innerText = "";
   humidityEl.innerText = "";
   windSpeedEl.innerText = "";
@@ -74,3 +98,18 @@ const navElements = document.querySelectorAll('.nav-element');
 navElements.forEach((element) => {
   element.addEventListener('click', activeNav);
 })
+
+if ('geolocation' in navigator) {
+  getLocation();
+} else {
+  log('No geolocation');
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    const coords = `${pos.coords.latitude},${pos.coords.longitude}`
+    getCurrentWeather(coords);
+  }, (err) => {
+    throw(new Error(err.message));
+  });
+}
